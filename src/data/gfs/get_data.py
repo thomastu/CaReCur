72295d00cbd41f9969c511b902bc12a8178d5f31
Thomas Tu <thomasthetu@gmail.com>
2020-04-08 03:25:46 -0700

    remove unused gfs thredds pipeline

--- src/data/gfs/get_data.py
+++
@@ -1,9 +0,0 @@
-"""The THREDDS catalog is a live GRIB service which enables access to filtered views.
-"""
-from siphon.catalog import TDSCatalog
-from siphon.ncss import NCSS
-
-dataset = list(gfs.datasets.values())[1]
-
-gfs = TDSCatalog('http://thredds.ucar.edu/thredds/catalog/grib/'
-                 'NCEP/GFS/Global_0p5deg/catalog.xml')
